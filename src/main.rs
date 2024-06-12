9d3d0a8fe3bbca67368ce1034d763740fb031c7b
Joshua Purushothaman <52509691+joshuaPurushothaman@users.noreply.github.com>
2024-06-12 06:05:15 -0500

    ADDED RUST PROGRAMMING LANGUAGE :)

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// package battleship
+
+// import "fmt"
+
+// type Vec2 struct {
+// 	X, Y uint8
+// }
+
+// type Ship struct {
+// 	Start Vec2
+// 	End   Vec2
+// }
+
+// type Cell struct {
+// 	BeenHit bool
+// }
+
+// type Board struct {
+// 	Ships [5]Ship
+// 	Cells [10][10]Cell
+// }
+
+// func MakeBoard() Board {
+// 	var b = Board{}
+// 	fmt.Println(b.Cells)
+
+// 	return b
+// }
+fn main() {
+    println!("Hello, world!");
+}
